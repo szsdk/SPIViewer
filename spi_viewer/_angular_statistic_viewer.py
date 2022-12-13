755886a2663d447224f9c3629fcaf602037dd1fd
szsdk <shen_zhou@u.nus.edu>
2022-12-13 10:37:32 +0100

    improve: code style

--- spi_viewer/_angular_statistic_viewer.py
+++ spi_viewer/_angular_statistic_viewer.py
@@ -1,6 +1,8 @@
-from ._pattern_data_model import PatternDataModel
-from ._ang_binned_statistic import ang_binned_statistic
 import pyqtgraph as pg
+
+from ._ang_binned_statistic import ang_binned_statistic
+from ._pattern_data_model import PatternDataModel
+
 
 class AngularStatisticViewer(pg.PlotWidget):
     def __init__(self, dm: PatternDataModel, bins=10, parent=None):
@@ -11,12 +13,7 @@ class AngularStatisticViewer(pg.PlotWidget):
 
     def updatePlot(self, pv):
         dm = pv.currentDataset
-        ans = ang_binned_statistic(
-            dm.patterns[dm.index],
-            dm.detector,
-            bins=self.bins
-        )
+        ans = ang_binned_statistic(dm.patterns[dm.index], dm.detector, bins=self.bins)
         self.dataLine.setData(
-            (ans.bin_edges[:-1] + ans.bin_edges[1:])/2,
-            ans.statistic
+            (ans.bin_edges[:-1] + ans.bin_edges[1:]) / 2, ans.statistic
         )
